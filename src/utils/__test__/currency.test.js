dd80684d1d0a55f3cac1dab2e0c6eb30ae39d882
geraldglynn <geraldglynn@gmail.com>
2020-09-01 16:40:04 +0100

    Added test for 'utils/currency'

--- src/utils/__test__/currency.test.js
+++ src/utils/__test__/currency.test.js
@@ -1,12 +1,46 @@
 import { currencySymbol } from 'utils/currency'
 
 describe('currency', () => {
+
   describe('currencySymbol', () => {
-    it('should return € as currency major if locale is IE', ()=> {
-      expect(currencySymbol('IE').major).toBe('€')
+
+    describe('EU countries', () => {
+      const contries = ['IE', 'ES', 'FR', 'DE']
+
+      contries.forEach( country => {
+        it(`should return € as currency major if locale in EU: ${country}`, ()=> {
+          expect(currencySymbol(country).major).toBe('€')
+        })
+        it(`should return ¢ as currency minor if locale is EU: ${country}`, ()=> {
+          expect(currencySymbol(country).minor).toBe('¢')
+        })
+      })
     })
-    it('should return ¢ as currency minor if locale is IE', ()=> {
-      expect(currencySymbol('IE').minor).toBe('¢')
+
+    describe('GB countries', () => {
+      const contries = ['GB', 'AI', 'BM', 'IO', 'GI']
+
+      contries.forEach( country => {
+        it(`should return € as currency major if locale in GB: ${country}`, ()=> {
+          expect(currencySymbol(country).major).toBe('£')
+        })
+        it(`should return ¢ as currency minor if locale is GB: ${country}`, ()=> {
+          expect(currencySymbol(country).minor).toBe('p')
+        })
+      })
+    })
+
+    describe('US countries', () => {
+      const contries = ['US']
+
+      contries.forEach( country => {
+        it(`should return € as currency major if locale in US: ${country}`, ()=> {
+          expect(currencySymbol(country).major).toBe('$')
+        })
+        it(`should return ¢ as currency minor if locale is US: ${country}`, ()=> {
+          expect(currencySymbol(country).minor).toBe('¢')
+        })
+      })
     })
   })
 })
